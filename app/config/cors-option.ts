3d7b5b918d78948cca126501cc9bae97bd0cf2be
Kathish kumaran <kathish@yavar.in>
2022-11-01 12:36:28 +0530

    changes

--- app/config/cors-option.ts
+++ app/config/cors-option.ts
@@ -3,7 +3,8 @@ const corsOptions = {
     methods: 'OPTION, GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,
-    exposedHeaders: 'Authorization'
+    exposedHeaders: 'Authorization',
+    allowedHeaders: 'Access-Control-Allow-Headers'
   };
   
   export default corsOptions;
