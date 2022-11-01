66968203e4ce398202fe1e15bd73b9cde5c08b33
Kathish kumaran <kathish@yavar.in>
2022-11-01 15:50:38 +0530

    changes

--- dist/services/customer.service.js
+++ dist/services/customer.service.js
@@ -107,8 +107,8 @@ function savePdf(base64) {
     //   throw new Error("Kindly upload only PNG or SVG file");
     // }
     const fileName = `${new Date().getTime()}.pdf`;
-    const filePath = `${__dirname}/../assets/${fileName}`;
-    //const filePath = `/app/dist/assets/${fileName}.pdf`;
+   // const filePath = `${__dirname}/../assets/${fileName}`;
+    const filePath = `/app/dist/assets/${fileName}.pdf`;
     // const base64 = (await (await file).toBuffer()).toString("base64");
     var buf = Buffer.from(base64, "base64");
     console.log("---------------", buf);
